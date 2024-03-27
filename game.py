069987c7583c368b1e399ab49190e6e596919cd2
a-maystorov <alkin971@gmail.com>
2024-03-27 21:11:13 +0100

    feat: create pygame window and respond to user input

---
+++ game.py
@@ -0,0 +1,31 @@
+import pygame
+import sys
+
+
+class Game:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1280, 720))
+        pygame.display.set_caption("Tower Defense Game")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible.
+            self.screen.fill((0, 155, 0))
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    td = Game()
+    td.run_game()
